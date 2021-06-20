64423f7acc8a4f0a47d73c85de1af7e3568be221
SIDD12124 <72179881+SIDD12124@users.noreply.github.com>
2021-06-20 15:17:22 +0530

    Add files via upload

---
+++ page2.js
@@ -0,0 +1,16 @@
+song = "";
+
+function preload(){
+    song = loadSound("song.mp3");
+}
+
+function play(){
+
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
+
+function next2(){
+    window.location = "page3.html";
+}
